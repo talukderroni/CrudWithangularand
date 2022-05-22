8da99a29f9b52ff337c6b8ef0986de0a308b3227
Yunus Ali Roni <yunus@NEXT_IT.com>
2022-05-22 10:24:25 +0600

    angular add

---
+++ AngularUI/src/app/app.route.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+export const appRoutes: Routes =
+ [
+    { path: 'home', component: HomeComponentComponent }
+
+ ];
+   
