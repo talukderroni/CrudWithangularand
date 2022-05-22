77c3e3a4a28741e53d2f8bbe09219b756336b34d
Yunus Ali Roni <yunus@NEXT_IT.com>
2022-05-22 10:37:13 +0600

    api added

---
+++ AngularApiApp/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AngularApiApp.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ AngularApiApp/Models/Salary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AngularApiApp.Models
+{
+    public partial class Salary
+    {
+        public int Id { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
