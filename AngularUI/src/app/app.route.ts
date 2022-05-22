05c22ed2821f5ffc465cea9cde8ac66fced04562
Talukder Roni <talukder.roni.ict@gmail.com>
2022-05-22 11:55:48 +0600

    xgdgf

--- AngularUI/src/app/app.route.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponentComponent } from './home-component/home-component.component';
-export const appRoutes: Routes =
- [
-    { path: 'home', component: HomeComponentComponent }
-
- ];
-   
